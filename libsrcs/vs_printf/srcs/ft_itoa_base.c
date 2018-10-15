3f8b291489c40aab63dda2c949ba524595c0159b
Jeremy CAMHI <jcamhi@student.42.fr>
2018-10-15 20:20:10 +0200

    Automatic commit

---
+++ libsrcs/vs_printf/srcs/ft_itoa_base.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsteffen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/01 15:57:01 by vsteffen          #+#    #+#             */
+/*   Updated: 2016/09/01 15:57:20 by vsteffen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char			*ft_itoa_base(uintmax_t nb, uint8_t base, size_t prec)
+{
+	char		*output;
+	char		*alph;
+	size_t		length;
+
+	if (nb == 0)
+		return (ft_strdup("0"));
+	length = (size_t)count_numeral_base(nb, base);
+	if (length < prec)
+		length = prec;
+	output = (char *)mallocp(sizeof(char) * length + 1);
+	output[length] = '\0';
+	alph = "0123456789abcdef";
+	while (nb != 0)
+	{
+		length--;
+		output[length] = alph[nb % base];
+		nb /= base;
+	}
+	if (length > 0)
+	{
+		while (--length > 0)
+			output[length] = '0';
+		output[0] = '0';
+	}
+	return (output);
+}
