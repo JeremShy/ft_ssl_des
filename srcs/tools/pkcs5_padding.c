0b87bd973ae9756ad54e32ba74ef092359187b47
Jeremy CAMHI <jcamhi@student.42.fr>
2018-10-31 21:48:42 +0100

    Changed malloc size, which was too small, causing several heap buffer overflows.

--- srcs/tools/pkcs5_padding.c
+++ srcs/tools/pkcs5_padding.c
@@ -15,7 +15,7 @@ uint8_t	*pkcs5_padding(const uint8_t *original_data, size_t *size, size_t padd_m
 	new_size = ((*size) / padd_multiple + 1) * padd_multiple;
 	if (new_size == *size)
 		new_size += padd_multiple;
-	ret = malloc(*size * sizeof(char));
+	ret = malloc(new_size * sizeof(char));
 	if (!ret)
 		return (NULL);
 	ft_memcpy(ret, original_data, *size);
