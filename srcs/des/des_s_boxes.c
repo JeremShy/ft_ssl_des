d591173c96542e6fe2cdfab1e223b1f38cb8c176
Jeremy CAMHI <jcamhi@student.42.fr>
2018-11-07 19:53:36 +0100

    ./srcs/des/des_s_boxes.c

--- srcs/des/des_s_boxes.c
+++ srcs/des/des_s_boxes.c
@@ -1,28 +1,28 @@
 #include <ft_ssl.h>
 
-static const int *get_s_box(size_t i)
+static const int	*get_s_box(size_t i)
 {
 	if (i == 0)
-		return g_des_one;
+		return (g_des_one);
 	else if (i == 1)
-		return g_des_two;
+		return (g_des_two);
 	else if (i == 2)
-		return g_des_three;
+		return (g_des_three);
 	else if (i == 3)
-		return g_des_four;
+		return (g_des_four);
 	else if (i == 4)
-		return g_des_five;
+		return (g_des_five);
 	else if (i == 5)
-		return g_des_six;
+		return (g_des_six);
 	else if (i == 6)
-		return g_des_seven;
+		return (g_des_seven);
 	else if (i == 7)
-		return g_des_eight;
+		return (g_des_eight);
 	else
-		return NULL;
+		return (NULL);
 }
 
-uint8_t	compute_s_box(uint8_t in, size_t i)
+uint8_t				compute_s_box(uint8_t in, size_t i)
 {
 	const int	*s_box;
 	uint8_t		row;
@@ -33,6 +33,5 @@ uint8_t	compute_s_box(uint8_t in, size_t i)
 	row = ((in & 0x20) >> 4) | (in & 1);
 	col = (in & 0x1E) >> 1;
 	out = s_box[row * 16 + col];
-	// ft_printf("out[%d] : %.4r\n", i, out);
 	return (out);
 }
