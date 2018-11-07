75462fdc7c167979d2c3e1c82223cd658181e4cd
Jeremy CAMHI <jcamhi@student.42.fr>
2018-11-07 20:09:35 +0100

    ./srcs/tools/hex_string_to_bytes.c

--- srcs/tools/hex_string_to_bytes.c
+++ srcs/tools/hex_string_to_bytes.c
@@ -4,13 +4,14 @@
 #define ZERO_BYTE -2
 
 /*
-	** transforms an ASCII representation of bytes (e.g. the c string "6061626360616263\0") into 
-	** into its size bytes representation (e.g. the byte arrray \x60\x61\x62\x63\x60\x61\x62\x63).
-	** If the str represents more than size bytes, it will be truncated so that out represents size bytes.
+	** transforms an ASCII representation of bytes (e.g. the c string
+		"6061626360616263\0") into
+	** into its size bytes representation (e.g. the byte arrray
+		\x60\x61\x62\x63\x60\x61\x62\x63).
+	** If the str represents more than size bytes, it will be
+		truncated so that out represents size bytes.
 	** If it represents less, it will be left-padded with zeros (\x00).
-
 	** out MUST be able to countains at least size bytes.
-
 	** returns one (1) if successful, zero (0) else.
 */
 
@@ -26,10 +27,10 @@ static uint8_t	compute_digit(char i)
 		return (ZERO_BYTE);
 	else
 		return (ERROR);
-
 }
 
-static uint8_t	compute_value(const char *str, unsigned char *out, size_t i, int8_t *ended)
+static uint8_t	compute_value(const char *str, unsigned char *out, size_t i,
+	int8_t *ended)
 {
 	char	d1;
 	char	d2;
@@ -55,7 +56,8 @@ static uint8_t	compute_value(const char *str, unsigned char *out, size_t i, int8
 	return (1);
 }
 
-int	hex_string_to_bytes(const char *str, unsigned char *out, size_t size)
+int				hex_string_to_bytes(const char *str,
+	unsigned char *out, size_t size)
 {
 	size_t	i;
 	int8_t	ended;
