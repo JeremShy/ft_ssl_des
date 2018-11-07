7457f7cc19867407f8640037c48ee95dee030b18
Jeremy CAMHI <jcamhi@student.42.fr>
2018-11-07 18:43:22 +0100

    srcs/bytes_to_key_md5/bytes_to_key_md5.c

--- srcs/bytes_to_key_md5/bytes_to_key_md5.c
+++ srcs/bytes_to_key_md5/bytes_to_key_md5.c
@@ -1,17 +1,50 @@
 #include <ft_ssl.h>
 
-// D_i = HASH^count(D_(i-1) || data || salt)
-// sizeof(D_I) = 16
+static int	do_round(int i, uint8_t *buffer, uint8_t *rez,
+	t_btk_md5_params *params)
+{
+	int		offset;
+	size_t	size_buffer;
 
-int	bytes_to_key_md5(t_btk_md5_params *params)
+	offset = 0;
+	if (i != 0)
+	{
+		offset = 16;
+		ft_memcpy(buffer, rez + (i - 1) * 16, 16);
+	}
+	ft_memcpy(buffer + offset, params->data, params->data_len);
+	if (params->salt != NULL)
+	{
+		ft_memcpy(buffer + offset + params->data_len, params->salt, 8);
+		size_buffer = offset + params->data_len + 8;
+	}
+	else
+		size_buffer = offset + params->data_len;
+	if (!ft_md5(buffer, size_buffer, rez + i * 16))
+	{
+		free(rez);
+		free(buffer);
+		return (0);
+	}
+	return (1);
+}
+
+int			calculate_required_rounds(t_btk_md5_params *params)
+{
+	int		required_rounds;
+
+	required_rounds = (params->key_len + params->iv_len) / (16);
+	if ((size_t)required_rounds * 16 != params->key_len + params->iv_len)
+		required_rounds++;
+	return (required_rounds);
+}
+
+int			bytes_to_key_md5(t_btk_md5_params *params)
 {
 	uint8_t	*buffer;
 	int		required_rounds;
 	int		i;
-	size_t	salt_len;
 	uint8_t	*rez;
-	int		offset;
-	size_t	size_buffer;
 
 	if (!(rez = malloc(params->key_len + params->iv_len)))
 		return (0);
@@ -20,37 +53,13 @@ int	bytes_to_key_md5(t_btk_md5_params *params)
 		free(rez);
 		return (0);
 	}
-	required_rounds = (params->key_len + params->iv_len) / (16);
-	if ((size_t)required_rounds * 16 != params->key_len + params->iv_len)
-		required_rounds++;
-	salt_len = 0;
-	if (params->salt != NULL)
-		salt_len = 8;
+	required_rounds = calculate_required_rounds(params);
 	i = 0;
-	offset = 0;
 	while (i < required_rounds)
 	{
-		if (i != 0)
-		{
-			offset = 16;
-			ft_memcpy(buffer, rez + (i - 1) * 16, 16);
-		}
-		ft_memcpy(buffer + offset, params->data, params->data_len);
-		if (params->salt != NULL)
-		{
-			ft_memcpy(buffer + offset + params->data_len, params->salt, 8);
-			size_buffer = offset + params->data_len + 8;
-		}
-		else
-			size_buffer = offset + params->data_len;
-		if (!ft_md5(buffer, size_buffer, rez + i * 16))
-		{
-			free(rez);
-			free(buffer);
+		if (!do_round(i, buffer, rez, params))
 			return (0);
-		}
 		i++;
-
 	}
 	ft_memcpy(params->key, rez, params->key_len);
 	ft_memcpy((void*)params->iv, rez + params->key_len, params->iv_len);
