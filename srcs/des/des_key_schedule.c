b76f1590ab1b6b5ad9ca15d9374d5730e41c51df
Jeremy CAMHI <jcamhi@student.42.fr>
2018-11-07 19:51:54 +0100

    srcs/des/des_key_schedule.c

--- srcs/des/des_key_schedule.c
+++ srcs/des/des_key_schedule.c
@@ -1,45 +1,5 @@
 #include <ft_ssl.h>
 
-void	print_block_as_hex(uint64_t in)
-{
-	printf("block : %02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx\n", (char)in, *(((char*)&in) + 1), *(((char*)&in) + 2), *(((char*)&in) + 3), *(((char*)&in) + 4), *(((char*)&in) + 5), *(((char*)&in) + 6), *(((char*)&in) + 7));
-}
-
-void	print_binary(uint8_t *in, size_t size, size_t blocks)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < size)
-	{
-		printf("%d", !!((in[i / 8] << (i % 8)) & 0x80));
-		i++;
-		if (i % blocks == 0)
-			printf(" ");
-	}
-}
-
-
-void print_half_key(uint64_t key)
-{
-	// print_block_as_hex(key);
-	print_binary((void*)&key, 28, 7);
-	printf("\n");
-}
-
-void print_key(uint64_t key)
-{
-	// print_block_as_hex(key);
-	print_binary((void*)&key, 64, 8);
-	printf("\n");
-}
-
-void print_48_key(t_uint48 key)
-{
-	print_binary((void*)&key, 48, 6);
-	printf("\n");
-}
-
 t_uint48	calculate_key(uint64_t c, uint64_t d)
 {
 	t_uint48	ret;
@@ -52,10 +12,10 @@ t_uint48	calculate_key(uint64_t c, uint64_t d)
 	return (ret);
 }
 
-void	compute_key_schedule(t_uint48 out[16], uint64_t key)
+void		compute_key_schedule(t_uint48 out[16], uint64_t key)
 {
-	uint32_t	c; // 28 bit
-	uint32_t	d; // 28 bit
+	uint32_t	c;
+	uint32_t	d;
 	size_t		i;
 
 	c = 0;
@@ -77,7 +37,5 @@ void	compute_key_schedule(t_uint48 out[16], uint64_t key)
 		}
 		i++;
 		out[i - 1] = calculate_key(c, d);
-		// printf("out[%2zu] : ", i - 1);
-		// print_48_key(out[i - 1]);
 	}
 }
