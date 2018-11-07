670d983d2c0a83049afd41597384c89253815d2b
Jeremy CAMHI <jcamhi@student.42.fr>
2018-11-07 20:04:46 +0100

    ./srcs/sha1/sha1_main.c

--- srcs/sha1/sha1_main.c
+++ srcs/sha1/sha1_main.c
@@ -1,16 +1,15 @@
 #include <ft_ssl.h>
 
-void	do_sha1(int in_fd, int out_fd, char *in_filename)
+void		do_sha1(int in_fd, int out_fd, char *in_filename)
 {
-	char	*file;
-	int		in_size;
+	char		*file;
+	int			in_size;
 	uint32_t	*out;
 	char		b[41];
 
 	file = get_file(in_fd, &in_size);
 	out = sha1_encode(file, in_size);
 	bytes_to_char(out, b, 20);
-
 	ft_putstr_fd(in_filename, out_fd);
 	ft_putstr_fd("= ", out_fd);
 	ft_putendl_fd(b, out_fd);
@@ -18,17 +17,26 @@ void	do_sha1(int in_fd, int out_fd, char *in_filename)
 	free(file);
 }
 
-int	main_sha1(t_opt *opt)
+static int	handle_i_opt(t_opt *opt)
+{
+	int	in_fd;
+
+	if (!opt->i_option)
+		return (0);
+	in_fd = open(opt->i_option, O_RDONLY);
+	if (in_fd == -1)
+		return (-1);
+	return (in_fd);
+}
+
+int			main_sha1(t_opt *opt)
 {
 	int	in_fd;
 	int	out_fd;
 
 	if (opt->flags & I_OPT)
 	{
-		if (!opt->i_option)
-			return (0);
-		in_fd = open(opt->i_option, O_RDONLY);
-		if (in_fd == -1)
+		if ((in_fd = handle_i_opt(opt)) == -1)
 			return (0);
 	}
 	else
@@ -43,6 +51,7 @@ int	main_sha1(t_opt *opt)
 	}
 	else
 		out_fd = 1;
-	do_sha1(in_fd, out_fd, (opt->flags & I_OPT && opt->i_option != NULL ? opt->i_option : "(stdin)"));
+	do_sha1(in_fd, out_fd, (opt->flags & I_OPT && opt->i_option != NULL ?
+		opt->i_option : "(stdin)"));
 	return (1);
 }
